db40f5a718fcadb61b5d34aa90774b4d9be17d32
britomtlean <leandromatosqn@gmail.com>
2026-07-20 07:32:40 -0300

    update catalogo 6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
 
                 {/* BUTTONS */}
                 <div className="mt-6 flex flex-col gap-3">
-                    <LinkButton href="/catalogo">
+                    <LinkButton href="/home">
                         <div className="flex justify-center items-center gap-4">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
